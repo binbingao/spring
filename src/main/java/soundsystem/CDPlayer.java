6f3782716b5864e1c375b48f64c4a0369d782af2
gao zhenbin <1483514847@qq.com>
2016-06-07 16:35:04 +0800

    aa

---
+++ src/main/java/soundsystem/CDPlayer.java
@@ -0,0 +1,30 @@
+package soundsystem;
+
+import javax.inject.Inject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CDPlayer implements MediaPlayer{
+	private CompactDisc cd;
+	
+	//@Autowired(required=false)
+	@Inject
+	public CDPlayer(CompactDisc cd){
+		this.cd=cd;
+	}
+	
+	public void play(){
+		cd.play();
+	}
+	
+	@Autowired
+	public void setCompactDisc(CompactDisc cd){
+		this.cd=cd;
+	}
+	@Autowired
+	public void insertDisc(CompactDisc cd){
+		this.cd=cd;
+	}
+}
--- src/test/java/soundsystem/CDPlayerTest.java
+++ src/test/java/soundsystem/CDPlayerTest.java
@@ -2,7 +2,9 @@ package soundsystem;
 
 import static org.junit.Assert.*;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.StandardOutputStreamLog;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -11,11 +13,25 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes=CDPlayerConfig.class)
 public class CDPlayerTest {
+	
+	@Rule
+	public final StandardOutputStreamLog log=new StandardOutputStreamLog();
+	
+	@Autowired
+	private CDPlayer player;
 	@Autowired
 	private CompactDisc cd;
 	
 	@Test
 	public void cdShouldNotBeNull(){
 		assertNotNull(cd);
+	}
+	
+	@Test
+	public void play(){
+		player.play();
+		//System.out.println("aaaa   "+log.getLog());
+		assertEquals("Playing Sgt. Pepper's Lonely Hearts Club Band by The Beatles", log.getLog());
+		
 	}
 }
