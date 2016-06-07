cdd4468d53ffac67c8f138ad5f2fc3ce9b22422d
gao zhenbin <1483514847@qq.com>
2016-06-07 15:02:01 +0800

    s

--- src/main/java/soundsystem/SgtPeppers.java
+++ src/main/java/soundsystem/SgtPeppers.java
@@ -1,9 +1,11 @@
 package soundsystem;
 
+import javax.inject.Named;
+
 import org.springframework.stereotype.Component;
 
-@Component("lonelyHeartsClub")
-@Named
+@Component
+@Named("lonelyHeartsClub")
 public class SgtPeppers implements CompactDisc{
 	private String title="Sgt. Pepper's Lonely Hearts CLub Band";
 	private String artist="The Beatles";
