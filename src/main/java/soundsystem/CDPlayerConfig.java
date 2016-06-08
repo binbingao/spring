bda1e56dba504131686f5042c1f84d2ebaf2d788
1483514847@qq.com <bin@bin-PC>
2016-06-08 20:36:02 +0800

    q

--- src/main/java/soundsystem/CDPlayerConfig.java
+++ src/main/java/soundsystem/CDPlayerConfig.java
@@ -3,14 +3,16 @@ package soundsystem;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
 //@ComponentScan
+@Import(CDConfig.class)
 public class CDPlayerConfig {
-	@Bean
+	/*@Bean
 	public CompactDisc sgtPeppers(){
 		return new SgtPeppers();
-	}
+	}*/
 	/*@Bean
 	public CDPlayer cdPlayer(){
 		return new CDPlayer(sgtPeppers());
